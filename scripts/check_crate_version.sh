b86b9509647824932e2e9d1171dbc03284450562
Dan Sover <dan.sover@avalabs.org>
2023-09-08 12:59:51 -0400

    fix: Simplify script
    Signed-off-by: Dan Sover <dan.sover@avalabs.org>

--- scripts/check_crate_version.sh
+++ scripts/check_crate_version.sh
@@ -1,41 +1,29 @@
 #!/bin/bash
 
 CRATE=$1
-if [[ $CRATE == "avalanche-types" ]]; then
-	echo "Checking avalanche-types"
 
-	avalanche_types_published_version=$(cargo search avalanche-types | head -1 | cut -d'"' -f 2)
-	echo $avalanche_types_published_version
+if [[ -z $CRATE ]]; then
+	echo "Please provide the crate name"
+	exit 1
+fi
 
-	avalanche_types_current_version=$(cargo pkgid -p avalanche-types | cut -d "#" -f2)
-	echo $avalanche_types_current_version
-
-	if [ $avalanche_types_published_version = $avalanche_types_current_version ]; then
-		echo "The current and published versions are equal."
-        exit 1
-	else
-		echo "The current and published versions are different."
-        exit 0
-	fi
-
-elif [[ $CRATE == "avalanche-consensus" ]]; then
-	echo "Checking avalanche-consensus"
-
-	avalanche_consensus_published_version=$(cargo search avalanche-consensus | head -1 | cut -d'"' -f 2)
-	echo $avalanche_consensus_published_version
-
-	avalanche_consensus_current_version=$(cargo pkgid -p avalanche-consensus | cut -d "#" -f2)
-	echo $avalanche_consensus_current_version
-
-	if [ $avalanche_consensus_published_version = $avalanche_consensus_current_version ]; then
-		echo "The current and published versions are equal."
-        exit 1
-	else
-		echo "The current and published versions are different."
-        exit 0
-	fi
-
-else
+if [[$CRATE != "avalanche-types" || $CRATE != "avalanche-consensus"]]; then
 	echo "Invalid crate name"
-    exit 1
+	exit 1
+fi
+
+echo "Checking $CRATE"
+
+published_version=$(cargo search $CRATE | head -1 | cut -d'"' -f 2)
+echo $published_version
+
+current_version=$(cargo pkgid -p $CRATE | cut -d "#" -f2)
+echo $avalanche_types_current_version
+
+if [ $published_version = $current_version ]; then
+	echo "The current and published versions are equal."
+	exit 1
+else
+	echo "The current and published versions are different."
+	exit 0
 fi
