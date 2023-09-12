c1b42c2f926d9198c6b01dac7d995ac75bc23bf2
sanghren <sanghren@bouh.io>
2023-09-12 08:23:55 +0200

    feat: some cleanup

--- core/network/src/p2p/gossip/mod.rs
+++ core/network/src/p2p/gossip/mod.rs
@@ -3,9 +3,6 @@ pub mod bloom;
 
 use avalanche_types::ids::Id;
 
-/// Represent the mempool of an avalanche-rs node.
-/// ref. <https://pkg.go.dev/github.com/ava-labs/avalanchego@v1.10.9/vms/platformvm/txs/mempool>
-
 pub trait Gossipable {
     fn get_id(&self) -> Id;
     fn marshal(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
